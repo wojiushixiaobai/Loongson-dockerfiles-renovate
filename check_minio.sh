833b396b2d639850c36e852655a831c681e32f73
吴小白 <296015668@qq.com>
2024-05-07 15:46:37 +0800

    feat: 添加 minio

---
+++ check_minio.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+CHECK_VERSION=${1:-debian}
+
+# github.com/minio/minio
+GITHUB_OWNER=minio
+GITHUB_REPO=minio
+# minio/minio
+CR_OWNER=${GITHUB_OWNER}
+CR_REPO=${GITHUB_REPO}
+
+APP_DIR=${CR_OWNER}/${CR_REPO}/${CHECK_VERSION}
+
+TMPDIR=$(mktemp -d)
+
+if [ ! -d "${TMPDIR}" ]; then
+    echo "Failed to create temporary directory"
+    exit 1
+fi
+
+if [ -f "${TMPDIR}/Dockerfile" ]; then
+    rm -f "${TMPDIR}/Dockerfile"
+fi
+
+DOCKERFILE_URL="https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/raw/master/Dockerfile.release"
+wget -qO "${TMPDIR}/Dockerfile" "${DOCKERFILE_URL}" || exit 1
+
+function get_go_version() {
+    cat "${TMPDIR}/Dockerfile" | grep -oP 'FROM golang:\K[0-9.]+'
+}
+
+function get_minio_version() {
+    curl -s "https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/releases/latest" | jq -r '.tag_name'
+}
+
+function get_mc_version() {
+    curl -s "https://api.github.com/repos/${GITHUB_OWNER}/mc/releases/latest" | jq -r '.tag_name'
+}
+
+function check_app_version() {
+    APP_VERSION=$1
+    if [ ! -f "${APP_DIR}/Dockerfile" ]; then
+        echo "No Dockerfile found for ${APP_DIR}"
+        exit 1
+    fi
+    if grep -q "ARG MINIO_VERSION=${APP_VERSION}" "${TMPDIR}/Dockerfile"; then
+        echo "latest"
+    fi
+}
+
+MINIO_VERSION=$(get_minio_version)
+MC_VERSION=$(get_mc_version)
+
+CHECK_LATEST_VERSION=$(check_app_version ${MINIO_VERSION})
+
+if [ "${CHECK_LATEST_VERSION}" == "latest" ]; then
+    echo "Latest version already exists"
+    exit 0
+fi
+
+GO_VERSION=$(get_go_version)
+ENV_VARS=("MINIO_VERSION" "MC_VERSION")
+
+for VAR in "${ENV_VARS[@]}"; do
+    VALUE=${!VAR}
+    sed -i "s|ARG ${VAR}=.*|ARG ${VAR}=${VALUE}|" "${APP_DIR}/Dockerfile"
+done
+
+sed -i "s|cr.loongnix.cn/library/golang:.*-buster|cr.loongnix.cn/library/golang:${GO_VERSION}-buster|" "${APP_DIR}/Dockerfile"
+sed -i "s|MINIO_VERSION?=.*$|MINIO_VERSION?=${MINIO_VERSION}|" "${APP_DIR}/Makefile"
+sed -i "s|MC_VERSION?=.*$|MC_VERSION?=${MC_VERSION}|" "${APP_DIR}/Makefile"
--- check_python.sh
+++ check_python.sh
@@ -25,33 +25,12 @@ fi
 DOCKERFILE_URL="https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/raw/master/${CHECK_VERSION}/slim-bullseye/Dockerfile"
 wget -qO "${TMPDIR}/Dockerfile" "${DOCKERFILE_URL}" || exit 1
 
-function get_app_version() {
-    grep -oP 'ENV PYTHON_VERSION \K.*' "${TMPDIR}/Dockerfile"
-}
-
-function get_gpg_key() {
-    grep -oP 'ENV GPG_KEY \K.*' "${TMPDIR}/Dockerfile"
-}
-
-function get_pip_version() {
-    grep -oP 'ENV PYTHON_PIP_VERSION \K.*' "${TMPDIR}/Dockerfile"
-}
-
-function get_setuptools_version() {
-    grep -oP 'ENV PYTHON_SETUPTOOLS_VERSION \K.*' "${TMPDIR}/Dockerfile"
-}
-
-function get_pip_url() {
-    grep -oP 'ENV PYTHON_GET_PIP_URL \K.*' "${TMPDIR}/Dockerfile"
-}
-
-function get_pip_sha() {
-    grep -oP 'ENV PYTHON_GET_PIP_SHA256 \K.*' "${TMPDIR}/Dockerfile"
+function get_env_var() {
+    grep -oP "ENV $1 \K.*" "${TMPDIR}/Dockerfile"
 }
 
 function check_app_version() {
     APP_VERSION=$1
-    # dockerfiles/library/python/3.9/debian/Dockerfile
     if [ ! -f "${APP_DIR}/Dockerfile" ]; then
         echo "No Dockerfile found for ${APP_DIR}"
         exit 1
@@ -61,24 +40,19 @@ function check_app_version() {
     fi
 }
 
-PYTHON_VERSION=$(get_app_version)
+PYTHON_VERSION=$(get_env_var PYTHON_VERSION)
 CHECK_LATEST_VERSION=$(check_app_version ${PYTHON_VERSION})
 
-if [ "${check_latest_version}" == "latest" ]; then
+if [ "${CHECK_LATEST_VERSION}" == "latest" ]; then
     echo "Latest version already exists"
     exit 0
 fi
 
-GPG_KEY=$(get_gpg_key)
-PYTHON_PIP_VERSION=$(get_pip_version)
-PYTHON_SETUPTOOLS_VERSION=$(get_setuptools_version)
-PYTHON_GET_PIP_URL=$(get_pip_url)
-PYTHON_GET_PIP_SHA256=$(get_pip_sha)
+ENV_VARS=("PYTHON_VERSION" "GPG_KEY" "PYTHON_PIP_VERSION" "PYTHON_SETUPTOOLS_VERSION" "PYTHON_GET_PIP_URL" "PYTHON_GET_PIP_SHA256")
 
-sed -i "s|ENV PYTHON_VERSION .*|ENV PYTHON_VERSION ${PYTHON_VERSION}|" "${APP_DIR}/Dockerfile"
-sed -i "s|ENV GPG_KEY .*|ENV GPG_KEY ${GPG_KEY}|" "${APP_DIR}/Dockerfile"
-sed -i "s|ENV PYTHON_PIP_VERSION .*|ENV PYTHON_PIP_VERSION ${PYTHON_PIP_VERSION}|" "${APP_DIR}/Dockerfile"
-sed -i "s|ENV PYTHON_SETUPTOOLS_VERSION .*|ENV PYTHON_SETUPTOOLS_VERSION ${PYTHON_SETUPTOOLS_VERSION}|" "${APP_DIR}/Dockerfile"
-sed -i "s|ENV PYTHON_GET_PIP_URL .*|ENV PYTHON_GET_PIP_URL ${PYTHON_GET_PIP_URL}|" "${APP_DIR}/Dockerfile"
-sed -i "s|ENV PYTHON_GET_PIP_SHA256 .*|ENV PYTHON_GET_PIP_SHA256 ${PYTHON_GET_PIP_SHA256}|" "${APP_DIR}/Dockerfile"
+for VAR in "${ENV_VARS[@]}"; do
+    VALUE=$(get_env_var ${VAR})
+    sed -i "s|ENV ${VAR} .*|ENV ${VAR} ${VALUE}|" "${APP_DIR}/Dockerfile"
+done
+
 sed -i "s|VERSION?=.*$|VERSION?=${PYTHON_VERSION}|" "${APP_DIR}/../Makefile"
